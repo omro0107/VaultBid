19e556b55410bee2845e82081a20b614bc64c2e6
omro0107 <142796898+omro0107@users.noreply.github.com>
2024-11-26 21:55:50 +0100

    restructuring an installed Vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: 'src', // Set the root directory to your source folder
+    build: {
+        outDir: '../dist', // Output directory for the build
+        emptyOutDir: true, // Clean the output directory before each build
+    },
+    server: {
+        port: 3000, // You can specify a port for the development server
+    },
+});
